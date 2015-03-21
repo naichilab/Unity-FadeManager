c40b7e9d07df65519b96cf451529a383039caedf
naichilab <naichilab@live.jp>
2015-03-22 02:34:21 +0900

    use the Texture2D.whiteTexture.

--- Assets/naichilab/FadeManager/Scripts/FadeManager.cs
+++ Assets/naichilab/FadeManager/Scripts/FadeManager.cs
@@ -5,7 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 /// <summary>
-/// シーン遷移時のフェードイン・アウトを制御するためのクラス
+/// シーン遷移時のフェードイン・アウトを制御するためのクラス .
 /// </summary>
 public class FadeManager : MonoBehaviour
 {
@@ -31,15 +31,16 @@ public class FadeManager : MonoBehaviour
 	#endregion Singleton
 
 	/// <summary>
-	/// デバッグモード
+	/// デバッグモード .
 	/// </summary>
 	public bool DebugMode = true;
-	/// <summary>暗転用黒テクスチャ</summary>
-	private Texture2D blackTexture;
 	/// <summary>フェード中の透明度</summary>
 	private float fadeAlpha = 0;
 	/// <summary>フェード中かどうか</summary>
 	private bool isFading = false;
+	/// <summary>フェード色</summary>
+	public Color fadeColor = Color.black;
+
 
 	public void Awake ()
 	{
@@ -49,53 +50,48 @@ public class FadeManager : MonoBehaviour
 		}
 		
 		DontDestroyOnLoad (this.gameObject);
-		
-		//ここで黒テクスチャ作る
-		this.blackTexture = new Texture2D (32, 32, TextureFormat.RGB24, false);
-		this.blackTexture.ReadPixels (new Rect (0, 0, 32, 32), 0, 0, false);
-		this.blackTexture.SetPixel (0, 0, Color.white);
-		this.blackTexture.Apply ();
 	}
 
 	public void OnGUI ()
 	{
 	
-		// Fade
-		if (this.isFading) {		
-			//透明度を更新して黒テクスチャを描画
-			GUI.color = new Color (0, 0, 0, this.fadeAlpha);
-			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), this.blackTexture);
+		// Fade .
+		if (this.isFading) {
+			//色と透明度を更新して白テクスチャを描画 .
+			this.fadeColor.a = this.fadeAlpha;
+			GUI.color = this.fadeColor;
+			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), Texture2D.whiteTexture);
 		}		
-		
 	
-		if (this.DebugMode && !this.isFading) {
-
-			//Scene一覧を作成
-			//(UnityEditor名前空間を使わないと自動取得できなかったので決めうちで作成)
-			List<string> scenes = new List<string> ();
-			scenes.Add ("SampleScene");
-			//scenes.Add ("SomeScene1");
-			//scenes.Add ("SomeScene2");
-
-
-			//Sceneが一つもない
-			if (scenes.Count == 0) {
-				GUI.Box (new Rect (10, 10, 200, 50), "Fade Manager(Debug Mode)");
-				GUI.Label (new Rect (20, 35, 180, 20), "Scene not found.");
-				return;
-			}
+		if (this.DebugMode) {
+			if (!this.isFading) {
+				//Scene一覧を作成 .
+				//(UnityEditor名前空間を使わないと自動取得できなかったので決めうちで作成) .
+				List<string> scenes = new List<string> ();
+				scenes.Add ("SampleScene");
+				//scenes.Add ("SomeScene1");
+				//scenes.Add ("SomeScene2");
 
 
-			GUI.Box (new Rect (10, 10, 300, 50 + scenes.Count * 25), "Fade Manager(Debug Mode)");
-			GUI.Label (new Rect (20, 30, 280, 20), "Current Scene : " + Application.loadedLevelName);
-
-			int i = 0;
-			foreach (string sceneName in scenes) {
-				if (GUI.Button (new Rect (20, 55 + i * 25, 100, 20), "Load Level")) {
-					LoadLevel (sceneName, 1.0f);
+				//Sceneが一つもない .
+				if (scenes.Count == 0) {
+					GUI.Box (new Rect (10, 10, 200, 50), "Fade Manager(Debug Mode)");
+					GUI.Label (new Rect (20, 35, 180, 20), "Scene not found.");
+					return;
 				}
-				GUI.Label (new Rect (125, 55 + i * 25, 1000, 20), sceneName);
-				i++;
+
+
+				GUI.Box (new Rect (10, 10, 300, 50 + scenes.Count * 25), "Fade Manager(Debug Mode)");
+				GUI.Label (new Rect (20, 30, 280, 20), "Current Scene : " + Application.loadedLevelName);
+
+				int i = 0;
+				foreach (string sceneName in scenes) {
+					if (GUI.Button (new Rect (20, 55 + i * 25, 100, 20), "Load Level")) {
+						LoadLevel (sceneName, 1.0f);
+					}
+					GUI.Label (new Rect (125, 55 + i * 25, 1000, 20), sceneName);
+					i++;
+				}
 			}
 		}
 		
@@ -104,7 +100,7 @@ public class FadeManager : MonoBehaviour
 	}
 
 	/// <summary>
-	/// 画面遷移
+	/// 画面遷移 .
 	/// </summary>
 	/// <param name='scene'>シーン名</param>
 	/// <param name='interval'>暗転にかかる時間(秒)</param>
@@ -114,13 +110,13 @@ public class FadeManager : MonoBehaviour
 	}
 
 	/// <summary>
-	/// シーン遷移用コルーチン
+	/// シーン遷移用コルーチン .
 	/// </summary>
 	/// <param name='scene'>シーン名</param>
 	/// <param name='interval'>暗転にかかる時間(秒)</param>
 	private IEnumerator TransScene (string scene, float interval)
 	{
-		//だんだん暗く
+		//だんだん暗く .
 		this.isFading = true;
 		float time = 0;
 		while (time <= interval) {
@@ -129,10 +125,10 @@ public class FadeManager : MonoBehaviour
 			yield return 0;
 		}
 		
-		//シーン切替
+		//シーン切替 .
 		Application.LoadLevel (scene);
 		
-		//だんだん明るく
+		//だんだん明るく .
 		time = 0;
 		while (time <= interval) {
 			this.fadeAlpha = Mathf.Lerp (1f, 0f, time / interval);
@@ -143,5 +139,4 @@ public class FadeManager : MonoBehaviour
 		this.isFading = false;
 	}
 }
-
 
